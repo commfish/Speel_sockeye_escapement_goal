ddbf7676f0cf4419b7e46b375f530255b9f7ab9b
Miller <sara.miller@dfg.alaska.local>
2019-12-09 14:05:40 -0900

    model source

--- code/model_source.R
+++ code/model_source.R
@@ -1,145 +1,113 @@
 #State Space Model Function
 
-# this function gets sourced from this file, and then 
-# written in JAGS format using write.model() 
-# This happens in the main script
-
-#Definitions:
-#Terminal Run = US Harvest + Abover Border Run
-#Above Border Run = Esc + Canadian Harvest + Test Fishery Harvest
-#Terminal Run = Esc + Canadian Harvest + Test Fishery Harvest + US Harvest
-#Esc = Above Border Run estimate -  Canadian Harvest
-
 #analysis----
 #State Space Model Function
-mod=function(){
-  for (y in (A+a.min):(Y+A-1)) {
-    log.R[y] ~ dnorm(log.R.mean2[y],tau.R)
-    R[y] <- exp(log.R[y])
-    log.R.mean1[y] <- log(S[y-a.max]) + lnalpha - beta * S[y-a.max] 
-    log.resid[y] <- log(R[y])-log.R.mean1[y]
+mod=function() {
+  #  RICKER STOCK-RECRUIT RELATIONSHIP WITH AR1 ERRORS;
+  #  R[y] IS THE TOTAL RETURN FROM BROOD YEAR y
+  #  Y=31; A=3; a.min=4; a.max=6;
+  #  THERE ARE A TOTAL OF Y+A-1 = 31 + 3 - 1 = 33 BROOD YRS REPRESENTED IN DATA (INCL FORECAST)
+  #  THE FIRST a.max = 6 DO NOT HAVE CORRESPONDING SPAWNING ABUNDANCES (BY 1977-1982)
+  #  THE REMAINING Y-a.min = 27 DO (BROOD YEARS A+a.min=7 - 33)  (BY 1983-2009)
+  
+  
+  for (c in A+a.min:C) {
+    log.R[c] ~ dnorm(log.R.mean2[c],tau.R)
+    R[c] <- exp(log.R[c])
+    log.R.mean1[c] <- log(S[c-a.max]) + lnalpha - beta * S[c-a.max] #eq.1
+    log.resid[c] <- log(R[c]) - log.R.mean1[c]
   }
-  log.R.mean2[A+a.min] <- log.R.mean1[A+a.min] + phi * log.resid.0 
-  for (y in (A+a.min+1):(Y+A-1)) {
-    log.R.mean2[y] <- log.R.mean1[y] + phi * log.resid[y-1]
+  log.R.mean2[A+a.min] <- log.R.mean1[A+a.min] + phi * log.resid.O #eq.2
+  for (c in A+a.min+1:C) {
+    log.R.mean2[c] <- log.R.mean1[c] + phi * log.resid[c-1]
   }
-  #PRIORS
-  lnalpha ~ dnorm(0,1.0E-6)%_%T(1.0E-6, )#uninformative;*
-  #normal distribution with mean 0 and large variance; constrained to be >0 since more biologically conservative (pg. 406)
-  beta ~ dnorm(0,1.0E-6)%_%T(1.0E-6,)   #uninformative; normal distrib; constrained to be >0 *          
-  phi ~ dnorm(0,1.0E-6)%_%T(-1, 1)#uninformative; btw -1 and 1
-  mean.log.RO ~ dnorm(0,1.0E-6)#uninformative (initial returns); mean of initial returns
-  tau.RO ~ dgamma(0.001,0.001)#uninformative (initial returns); variance of initial returns
-  log.resid.0 ~ dnorm(0,tau.red)#model residual with AR(1) process error
-  tau.R ~ dgamma(0.001,0.001) #uninformative
+  
+  lnalpha ~ dnorm(0,1.0E-6)%_%I(0,)
+  beta ~ dnorm(0,1.0E-6)%_%I(0,)              
+  phi ~ dnorm(0,1.0E-6)%_%I(-1,1)
+  mean.log.RO ~ dnorm(0,1.0E-6)
+  tau.RO ~ dgamma(0.1,0.1)  
+  log.resid.O ~ dnorm(0,tau.red)
+  tau.R ~ dgamma(0.001,0.001) 
   
   sigma.R <- 1 / sqrt(tau.R)
   alpha <- exp(lnalpha)
-  sigma.RO <- 1 / sqrt(tau.RO)#uninformative; informative (not shown) prior had large effect on parameter itself 
-  #and initial R values, but not on key model quantities
+  sigma.RO <- 1 / sqrt(tau.RO)
   tau.red <- tau.R * (1-phi*phi)
-  lnalpha.c <- lnalpha + (sigma.R * sigma.R / 2 / (1-phi*phi) ) 
+  lnalpha.c <- lnalpha + (sigma.R * sigma.R / 2 / (1-phi*phi) ) #eq.22
+  S.max <- 1 / beta #eq.20
   
-  #THE FIRST SEVERAL COHORTS ORIGINATE FROM UNMONITORED SPAWNING EVENTS
-  #DRAW THESE RETURNS FROM A COMMON LOGNORMAL DISTRIBUTION 
+  
+  # THE FIRST SEVERAL COHORTS ORIGINATE FROM UNMONITORED SPAWNING EVENTS
+  # DRAW THESE RETURNS FROM A COMMON LOGNORMAL DISTRIBUTION #eq. 17-21
   R.O<-exp(mean.log.RO)
-  for (y in 1:a.max) { 
-    log.R[y] ~ dnorm(mean.log.RO,tau.RO) 
-    R[y] <- exp(log.R[y]) 
+  for (c in 1:a.max) { 
+    log.R[c] ~ dnorm(mean.log.RO,tau.RO) 
+    R[c] <- exp(log.R[c]) 
   }
-  #REFERENCE POINTS (WITH CORRECTION FOR LOGNORMAL SKEWNESS)
-  S.max <- 1 / beta 
+  #CORRECTION FOR LOGNORMAL SKEWNESS
   alpha.c <- min(exp(lnalpha.c),1.0E4)
-  S.eq.c <- lnalpha.c * S.max #Eq.21
-  U.msy.c <- lnalpha.c * (0.5-0.07*lnalpha.c)
-  S.msy.c <- S.eq.c *(0.5-0.07*lnalpha.c)  
-  
   positive.lna.c <- step(lnalpha.c)
   lnalpha.c.nonneg <- lnalpha.c * positive.lna.c
-  S.eq.c2 <- lnalpha.c.nonneg * S.max 
+  S.eq.c <- lnalpha.c.nonneg * S.max
   peterman.approx.c <- (0.5 - 0.65*pow(lnalpha.c.nonneg,1.27) / (8.7 +pow(lnalpha.c.nonneg,1.27)))
-  U.msy.c2 <- lnalpha.c.nonneg * peterman.approx.c 
-  S.msy.c2 <- U.msy.c2 / beta  
-  U.max.c2 <- 1 - 1 / exp(lnalpha.c.nonneg) 
-  S.msy.c.80 <- S.msy.c *0.80
-  S.msy.c2.80 <- S.msy.c2 *0.80
+  U.msy.c <- lnalpha.c.nonneg * peterman.approx.c #peterman approximation
+  S.msy.c <- U.msy.c / beta
+  U.max.c <- 1 - 1 / exp(lnalpha.c.nonneg)
   
-  #GENERATE Y+A-1 = 42 MATURITY SCHEDULES, ONE PER BROOD YEAR USING THE DIRICHLET DISTRIB. (Eq.4-6)
-  # "pi" (central tendency of "p"), and "D.scale" (dispersion of "p")
-  D.scale ~ dunif(0,1)#uninformative
-  D.sum <- 1 / (D.scale * D.scale)
-  pi.2p ~ dbeta(1,1)#uninformative
-  pi.1 ~ dbeta(1,1)#uninformative; Eq.6
-  pi[1] <- pi.1
+  # GENERATE Y+A-1 = 33 MATURITY SCHEDULES, ONE PER BROOD YEAR (eq.4-6)
+  D.scale ~ dunif(0,1)
+  D <- 1 / (D.scale * D.scale)
+  pi[1] ~ dbeta(0.2,0.8)#eq.6
+  pi.2p ~ dbeta(0.2,0.6)
   pi[2] <- pi.2p * (1 - pi[1])
-  pi[3] <- 1 - pi[1] - pi[2]
-  
+  pi[3] <- (1 - pi[1] - pi[2])
   
   for (a in 1:A) {
-    gamma[a] <- D.sum * pi[a]
-    for (y in 1:(Y+A-1)) {                                                    
-      g[y,a] ~ dgamma(gamma[a],0.01)
-      p[y,a] <- g[y,a]/sum(g[y,])
+    gamma[a] <- D * pi[a]#eq.6
+    for (c in 1:C) {                                                    
+      g[c,a] ~ dgamma(gamma[a],0.1)#eq.5
+      p[c,a] <- g[c,a]/sum(g[c,])#eq.4
     }
   }
   
-  #CALCULATE THE NUMBERS AT AGE MATRIX (Number returning to spawn at age in year y); Eq.3
-  #Product of the total return from brood year y-a and the prop. mature from cohort y-a returning at age a; 
+  
+  # CALCULATE THE NUMBERS AT AGE MATRIX
   for(a in 1:A){
     for(y in a:(Y+(a-1))){
-      N.ya[y-(a-1),(A+1-a)]<-p[y,(A+1-a)]*R[y]
+      N.ya[y-(a-1),(A+1-a)]<-R[y]*p[y,(A+1-a)] #eq.3
     }
   }
   
-  #MULTINOMIAL SCALE SAMPLING ON TOTAL ANNUAL RETURN N; Eq.13
+  # MULTINOMIAL SCALE SAMPLING ON TOTAL ANNUAL RETURN N #eq.13
   for (y in 1:Y) {
     N[y] <- sum(N.ya[y,1:A])
     for (a in 1:A) {
       q[y,a] <- N.ya[y,a] / N[y]
     }
-  } 
-  for (t in 1:Y){  
-    x[t,1:A] ~ dmulti(q[t,],n.a[t])
-  }  
+    n[y] <- sum(x[y,1:A])
+    x[y,1:A] ~ dmulti(q[y,],n[y])
+  }
   
   
-  #INRIVER RUN
+  # HARVESTS BELOW (No harvest above)#eq.13
+  # ASSUME 10% CV FOR H^BELOW
+  #Must now use hierarchical harvest rates
+  B.scale ~ dunif(0,1)
+  mu ~ dbeta(0.1,0.1)
+  B.sum<-1/B.scale/B.scale
+  B[1]<-mu*B.sum
+  B[2]<-B.sum-B[1]
   for (y in 1:Y) {
-    mu.hbelow_wild[y] ~ dbeta(1, 1)
-    h.below_wild[y] <- mu.hbelow_wild[y] * N[y]
-    log.hb_wild[y] <- log(h.below_wild[y])
-    tau.log.hb.wild[y] <- 1/log(cv.hb[y] * cv.hb[y] + 1)
-    hbelow_wild[y] ~ dlnorm(log.hb_wild[y], tau.log.hb.wild[y])
-    
-    mu.habove_wild[y] ~ dbeta(1, 1)
-    h.above_wild[y] <- mu.habove_wild[y] * N[y]
-    log.ha_wild[y] <- log(h.above_wild[y])
-    tau.log.ha.wild[y] <- 1/log(cv.ha[y] * cv.ha[y] + 1)
-    habove_wild[y] ~ dlnorm(log.ha_wild[y], tau.log.ha.wild[y])
-    inriver.run[y] <- max(N[y] - h.below_wild[y], 1) # inriver run (mr) or above boder estimate = terminal run  - harvest below the border (wild) [no personal use wild included]+ harvest above border (wild)  +  broodstock (wild) 
-    
-    #IN SOME YEARS, DIRECT ESTIMATES OF INRIVER RUN ABUNDANCE THROUGH MARK-RECAPTURE ESTIMATES
-    log.ir[y] <- log(inriver.run[y])             
-    tau.log.ir[y] <- 1 / log(cv.ir[y]*cv.ir[y] + 1)     
-    ir[y] ~ dlnorm(log.ir[y],tau.log.ir[y])  # mark-recapture data        
-    
-    mu.habove[y] ~ dbeta(1,1)                                  
-    h.above[y] <- mu.habove[y] * inriver.run[y]     
-    log.ha[y] <- log(h.above[y])     
-    tau.log.ha[y] <- 1 / log(cv.ha[y]*cv.ha[y] + 1)     
-    habove[y] ~ dlnorm(log.ha[y],tau.log.ha[y]) 
-    
-    mu.hbelow[y] ~ dbeta(1, 1)
-    h.below[y] <- mu.hbelow[y] * N[y]
-    log.hb[y] <- log(h.below[y])
-    tau.log.hb[y] <- 1 / log(cv.hb[y]*cv.hb[y] + 1)   
-    hbelow[y] ~ dlnorm(log.hb[y], tau.log.hb[y])
-    
-    mu[y] <- (h.below[y] + h.above[y]) / N[y]     
-    S[y] <- max(inriver.run[y] - h.above[y], 1)
-    #escapement = inriver - total harvest above (wild + enh)- broodstock (enh, wild)
-    log.S[y] <- log(S[y]) 
-    lnRS[y] <-log(R[y]/S[y]) 
+    mu.HB[y] ~ dbeta(B[1],B[2])
+    H.B[y] <- mu.HB[y] * N[y]
+    log.HB[y] <- log(H.B[y])
+    tau.log.hb[y] <- 1 / log(cv.hb[y]*cv.hb[y]+1)  
+    h.b[y] ~ dlnorm(log.HB[y],tau.log.hb[y])     
+    W[y]<-max(N[y]-H.B[y],1)#eq. 8
+    log.W[y] <- log(W[y])
+    tau.log.w[y] <- 1 / log(cv.w[y]*cv.w[y]+1)  
+    w[y]~dlnorm(log.W[y],tau.log.w[y])
+    S[y] <- W[y]#eq.9
+  }
 }
-}
-
